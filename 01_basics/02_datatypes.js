2eaa86c46b2b63b7515623becb863633ce99b3b1
Payl Nayak <166982413+Dynamicpayal@users.noreply.github.com>
2024-10-20 07:11:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // We are using nodejs, not browser
+
+let name = "Payal"
+let age = 18
+let isLoggedIn = true
+let state; 
+
+
+// number -> 2 power 53
+// bigint
+// string -> ""
+// boolean -> yes/no
+// null -> standalone value
+// undefined 
+// symbol
+
+console.log(typeof "Payal"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
