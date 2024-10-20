976821ca31b229d18cb07ee1a9aabc0571c61300
Payl Nayak <166982413+Dynamicpayal@users.noreply.github.com>
2024-10-20 11:29:07 +0000

    operations on js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,37 @@ let someNumber = 45
 let stringNumber = String(someNumber)
 console.log(stringNumber); // 45
 console.log(typeof stringNumber); // string
+
+// **************************** Operations ****************************
+
+let value = 5
+let negValue = -value
+console.log(negValue); // -5
+
+// console.log(5+2); // -5
+// console.log(7-4); // 7
+// console.log(8*5); // 3
+// console.log(10/4); // 40
+// console.log(2**3); // 8
+// console.log(64%2); // 0
+
+let str1 = "hello"
+let str2 = " JS"
+
+console.log(str1 + str2); // hello JS
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log( (3 + 4) * 8 % 3); // 2
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let gameCounter = 500
+gameCounter++;
+console.log(gameCounter); // 501
+
